8feb451e78e416a41ba6b559cd7253b2c7d9c870
Harshvardhan151 <157158971+Harshvardhan151@users.noreply.github.com>
2025-09-15 10:15:24 +0530

    Update 20.tuplemethods.py

---
+++ 20.tuplemethods.py
@@ -0,0 +1,18 @@
+# tuples are immutable so you cant make changes // to make changes convert it to a list
+
+countries = ("Brazil","Russia","India","China")
+temp = list(countries)
+print(temp)
+temp.append("South Africa")
+print(temp)
+temp.pop(2)
+print(temp)
+temp[2] = "Nippon"
+print(temp)
+countries = tuple(temp)
+print(countries)
+
+# methods in tuple
+
+marks = (41,67,41,67,41,67)
+print(marks.count(41))
